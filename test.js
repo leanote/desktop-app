8a989b37267453a1c47983a860af6af9e99afe9e
life <lifephp@gmail.com>
2015-01-20 10:41:03 +0800

    api

--- test.js
+++ test.js
@@ -2,6 +2,7 @@
 
 var Notebook = require('notebook');
 var Note = require('note');
+var Api = require('api');
 
 /*
 Notebook.addNotebook("1", "life");
@@ -10,4 +11,7 @@ Notebook.addNotebook("3", "工作");
 Notebook.addNotebook("4", "life2", "1");
 */
 
-Notebook.reCountNotebookNumberNotes('54bb2e89c596f2239a000000');
+// Notebook.reCountNotebookNumberNotes('54bb2e89c596f2239a000000');
+
+
+Api.uploadImage();
