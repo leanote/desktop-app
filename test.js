218671f53a77addabc290c6d6133302a171e892f
life <lifephp@gmail.com>
2015-01-26 23:42:56 +0800

    send changes note.update [ok]

--- test.js
+++ test.js
@@ -3,6 +3,7 @@
 var Notebook = require('notebook');
 var Note = require('note');
 var Api = require('api');
+var User = require('user');
 var Server = require('server');
 
 /*
@@ -10,11 +11,21 @@ Notebook.addNotebook("1", "life");
 Notebook.addNotebook("2", "生活");
 Notebook.addNotebook("3", "工作");
 Notebook.addNotebook("4", "life2", "1");
-*/
 
 Api.addNotebook({
   Title: "哈哈"
 }, function() {});
+*/
+User.userId = '54bdc65599c37b0da9000002';
+console.log(User.getCurActiveUserId());
+Note.getDirtyNotes(function(ret) {
+  console.log(ret);
+});
+/*
+Note.getNoteByServerNoteId("54c6313799c37bdeec000008", function(ret){ 
+  console.log(ret);
+});
+*/
 
 
 // Api.auth('c@a.com', 'abc123');
