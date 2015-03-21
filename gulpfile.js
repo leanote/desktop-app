4f4cfc1d4a8259ddca0877491ccf2e1e66adbcb1
life <lifephp@gmail.com>
2015-03-21 23:21:07 +0800

    nw

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,57 @@
 var gulp = require('gulp');
 var rename = require("gulp-rename");
-var del = require('del');
 var runSequence = require('run-sequence');
 var clean = require('gulp-clean');
 
-// 复制nw
+//-----------
+// mac
+//-----------
 
-gulp.task('cl', function () {
-	return gulp.src('dist').pipe(clean({force: true}));
+// 清理
+gulp.task('clMac', function () {
+	return gulp.src('dist/mac').pipe(clean({force: true}));
 });	
-gulp.task('cpNw', function () {
-	// del(['dist/mac'], function (err, deletedFiles) {
+gulp.task('cpMacNw', function () {
 	gulp.src('nw/mac/nw.app/**/*').pipe(gulp.dest('dist/mac/leanote.app'));
-	// });
 });
 
 // 复制源文件
-gulp.task('cpSrc', function () {
-	// 复制文件到app中
+gulp.task('cpMacSrc', function () {
+	// mac
 	gulp.src('src/public/**/*').pipe(gulp.dest('dist/mac/leanote.app/Contents/Resources/app.nw/public'));
 	gulp.src('src/node_modules/**/*').pipe(gulp.dest('dist/mac/leanote.app/Contents/Resources/app.nw/node_modules'));
 	gulp.src('src/package_mac.json').pipe(rename("package.json")).pipe(gulp.dest('dist/mac/leanote.app/Contents/Resources/app.nw/'));
 	gulp.src(['src/note.html', 'src/login.html']).pipe(gulp.dest('dist/mac/leanote.app/Contents/Resources/app.nw/'));
+});
 
-	/*
-	gulp.src('app/styles/main.css').pipe(gulp.dest('dist/styles/'));
-	gulp.src('app/scripts/jquery.treeview/jquery.treeview.css').pipe(gulp.dest('dist/styles/'))
-	gulp.src('app/scripts/jquery.treeview/images/*').pipe(gulp.dest('dist/styles/images/'))
-	gulp.src(['app/question.html', 'app/index.html', 'app/feedback.html', 'app/search.html', 'app/cate.html', 'app/search.html']).pipe(gulp.dest('dist/'))
-	gulp.src('app/images/*').pipe(gulp.dest('dist/images'))
-	gulp.src('app/scripts/require.js').pipe(gulp.dest('dist/scripts/'))
-	*/
+//------------
+// windows
+//------------
+
+gulp.task('clWindows', function () {
+	return gulp.src('dist/windows').pipe(clean({force: true}));
+});	
+gulp.task('cpWindowsNw', function () {
+	gulp.src('nw/windows/**/*').pipe(gulp.dest('dist/windows/'));
+});
+
+gulp.task('cpWindowsSrc', function () {
+	// windows
+	gulp.src('src/public/**/*').pipe(gulp.dest('dist/windows/public'));
+	gulp.src('src/node_modules/**/*').pipe(gulp.dest('dist/windows/node_modules'));
+	gulp.src('src/package_windows.json').pipe(rename("package.json")).pipe(gulp.dest('dist/windows/'));
+	gulp.src(['src/note.html', 'src/login.html']).pipe(gulp.dest('dist/windows/'));
+});
+
+// mac & windows
+gulp.task('mac', function(cb) {
+	runSequence('clMac', 'cpMacNw', 'cpMacSrc', cb);
+});
+
+gulp.task('windows', function(cb) {
+	runSequence('clWindows', 'cpWindowsNw', 'cpWindowsSrc', cb);
 });
 
 gulp.task('default', function(cb) {
-	runSequence('cl', 'cpNw', 'cpSrc', cb);
+	runSequence('mac', 'windows', cb);
 });
