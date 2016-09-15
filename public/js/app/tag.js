912bfbe161fb5dc88501d52f651f15be3b7d52be
LucasYuNju <LucasYuNju@gmail.com>
2016-09-15 21:36:11 +0800

    allow user to delete last tag by typing backspace

--- public/js/app/tag.js
+++ public/js/app/tag.js
@@ -1,344 +1,348 @@
-// // Tag
-// 
-// Tag.classes = {
-// 	"蓝色": "label label-blue",
-// 	"红色": "label label-red",
-// 	"绿色": "label label-green",
-// 	"黄色": "label label-yellow",
-// 	"blue": "label label-blue",
-// 	"red": "label label-red",
-// 	"green": "label label-green",
-// 	"yellow": "label label-yellow"
-// }
-// 
-// // 数据库中统一存En
-// Tag.mapCn2En = {
-// 	"蓝色": "blue",
-// 	"红色": "red",
-// 	"绿色": "green",
-// 	"黄色": "yellow",
-// }
-// Tag.mapEn2Cn = {
-// 	"blue": "蓝色",
-// 	"red": "红色",
-// 	"green": "绿色",
-// 	"yellow": "黄色",
-// }
-// Tag.isColorTag = function(tag) {
-// 	return tag == 'blue' || tag == 'red' || tag == 'green' || tag == 'yellow';
-// }
-// 
-// Tag.t = $("#tags");
-// 
-// // called by Note
-// Tag.getTags = function() {
-// 	var tags = [];
-// 	Tag.t.children().each(function(){
-// 		var text = $(this).data('tag');
-// 		text = Tag.mapCn2En[text] || text;
-// 		tags.push(text);
-// 	});
-// 	// 需要去重吗? 正常情况下不会重复
-// 	return tags;
-// }
-// 
-// // called by Note
-// Tag.clearTags = function() {
-// 	Tag.t.html("");
-// }
-// 
-// // 设置tags
-// // called by Note
-// Tag.renderTags = function(tags) {
-// 	Tag.t.html("");
-// 	if(isEmpty(tags)) {
-// 		return;
-// 	}
-// 	// TODO 重构, 这样不高效
-// 	for(var i = 0; i < tags.length; ++i) {
-// 		var tag = tags[i];
-// 		Tag.appendTag(tag);
-// 	}
-// };
-// 
-// function hideTagList(event) {
-// 	$("#tagDropdown").removeClass("open");
-// 	if (event) {
-// 		event.stopPropagation()
-// 	}
-// }
-// function showTagList(event) {
-// 	$("#tagDropdown").addClass("open");
-// 	if (event) {
-// 		event.stopPropagation()
-// 	}
-// }
-// 
-// // 添加tag
-// // tag = {classes:"label label-red", text:"红色"}
-// // tag = life
-// Tag.appendTag = function(tag, save) {
-// 	var isColor = false;
-// 	var classes, text;
-// 	
-// 	if (typeof tag == "object") {
-// 		classes = tag.classes;
-// 		text = tag.text;
-// 		if(!text) {
-// 			return;
-// 		}
-// 	} else {
-// 		tag = $.trim(tag);
-// 		text = tag;
-// 		if(!text) {
-// 			return;
-// 		}
-// 		var classes = Tag.classes[text];
-// 		if(classes) {
-// 			isColor = true;
-// 		} else {
-// 			classes = "label label-default";
-// 		}
-// 	}
-// 	var rawText = text;
-// 	if(Tag.isColorTag(text)) {
-// 		text = getMsg(text);
-// 	}
-// 
-// 	text = trimTitle(text);
-// 
-// 	tag = tt('<span class="?" data-tag="?">?<i title="' + getMsg("delete") + '">X</i></span>', classes, rawText, text);
-// 
-// 	// 避免重复
-// 	var isExists = false;
-// 	$("#tags").children().each(function() {
-// 		if (isColor) {
-// 			var tagHtml = $("<div></div>").append($(this).clone()).html();
-// 			if (tagHtml == tag) {
-// 				$(this).remove();
-// 				isExists = true;
-// 			}
-// 		} else if (text + "X" == $(this).text()) {
-// 			$(this).remove();
-// 			isExists = true;
-// 		}
-// 	});
-// 
-// 	$("#tags").append(tag);
-// 
-// 	hideTagList();
-// 
-// 	if (!isColor) {
-// 		reRenderTags();
-// 	}
-// 	
-// 	if(save) {
-// 		// 如果之前不存, 则添加之
-// 		if(!isExists) {
-// 			Note.curChangedSaveIt(true, function() {
-// 				TagService.addOrUpdateTag(rawText, function(ret) {
-// 					if(typeof ret == 'object' && ret.Ok !== false) {
-// 						Tag.addTagNav(ret);
-// 					}
-// 				});
-// 			});
-// 		}
-// 	}
-// };
-// 
-// // nodejs端调用
-// Tag.addTagsNav = function(tags) {
-// 	tags = tags || [];
-// 	for(var i = 0; i <  tags.length; ++i) {
-// 		Tag.addTagNav(tags[i]);
-// 	}
-// };
-// 
-// // 删除, nodejs调用
-// Tag.deleteTagsNav = function(tags) {
-// 	tags = tags || [];
-// 	for(var i = 0; i <  tags.length; ++i) {
-// 		var title = tags[i];
-// 		$('#myTag li[data-tag="' + title + '"]').remove();
-// 	}
-// };
-// 
-// // 为了颜色间隔, add, delete时调用
-// function reRenderTags() {
-// 	var defautClasses = [ "label label-default", "label label-info" ];
-// 	var i = 0;
-// 	$("#tags").children().each(
-// 		function() {
-// 			var thisClasses = $(this).attr("class");
-// 			if (thisClasses == "label label-default"
-// 					|| thisClasses == "label label-info") {
-// 				$(this).removeClass(thisClasses).addClass(
-// 						defautClasses[i % 2]);
-// 				i++;
-// 			}
-// 		});
-// };
-// 
-// // 删除tag
-// Tag.removeTag = function($target) {
-// 	var tag = $target.data('tag');
-// 	$target.remove();
-// 	reRenderTags();
-// 	Note.curChangedSaveIt(true, function() {
-// 		TagService.addOrUpdateTag(tag, function(ret) {
-// 			if(typeof ret == 'object' && ret.Ok !== false) {
-// 				Tag.addTagNav(ret);
-// 			}
-// 		});
-// 	});
-// }; 
-// 
-// //-----------
-// // 左侧nav en -> cn
-// Tag.tags = [];
-// Tag.renderTagNav = function(tags) {
-// 	var me = this;
-// 	tags = tags || [];
-// 	Tag.tags = tags;
-// 	$("#tagNav").html('');
-// 	for(var i in tags) {
-// 		var noteTag = tags[i];
-// 		var tag = noteTag.Tag;
-// 		var text = tag;
-// 
-// 		if(Tag.isColorTag(text)) {
-// 			text = getMsg(text);
-// 		}
-// 		
-// 		text = trimTitle(text);
-// 		var classes = Tag.classes[tag] || "label label-default";
-// 		// 笔记数量先隐藏, 不准确
-// 		$("#tagNav").append(tt('<li data-tag="?"><a> <span class="?">? <em style="display: none">(?)</em></span> <i class="tag-delete">X</i></li>', tag, classes, text, noteTag.Count));
-// 	}
-// 
-// 	if(tags.length == 0) {
-// 		$("#tagNav").html('<p class="no-info">' + getMsg('No tag') + '</p>');
-// 	}
-// };
-// 
-// // 添加的标签重新render到左边, 放在第一个位置
-// Tag.addTagNav = function(newTag) {
-// 	var me = this;
-// 	for(var i in me.tags) {
-// 		var noteTag = me.tags[i];
-// 		if(noteTag.Tag == newTag.Tag) {
-// 			me.tags.splice(i, 1);
-// 			break;
-// 		}
-// 	}
-// 	me.tags.unshift(newTag);
-// 	me.renderTagNav(me.tags);
-// };
-// 
-// Tag.searchTag = function(tag, noteId) {
-// 	// 1
-// 	Note.curChangedSaveIt();
-// 	
-// 	// 2 先清空所有
-// 	// 也会把curNoteId清空
-// 	Note.clearAll();
-// 	
-// 	var tagCn = tag;
-// 	if(Tag.isColorTag(tag)) {
-// 		tagCn = getMsg(tag);
-// 	}
-// 
-// 	var tagTitle = '<span class="' + (Tag.classes[tag] || 'label label-default') + '">' + trimTitle(tagCn) + '</span>';
-// 
-// 	Notebook.changeCurNotebookTitle(tagTitle, false, '', true);
-// 	Tag.curTag = tag;
-// 	
-// 	NoteService.searchNoteByTag(tag, function(notes) {
-// 		hideLoading();
-// 		if(notes) {
-// 			// 和note搜索一样
-// 			// 设空, 防止发生上述情况
-// 			// Note.curNoteId = "";
-// 			Note.renderNotes(notes);
-// 			Note.renderNotesAndTargetNote(notes, noteId);
-// 		}
-// 	});
-// };
-// 
-// 
-// // 事件
-// $(function() {
-// 	// tag input box
-// 	$("#addTagTrigger").click(function() {
-// 		$(this).hide();
-// 		$("#addTagInput").show().focus().val("");
-// 	});
-// 	
-// 	$("#addTagInput").click(function(event) {
-// 		showTagList(event);
-// 	});
-// 	
-// 	$("#addTagInput").blur(function() {
-// 		var val = $(this).val();
-// 		if(val) {
-// 			Tag.appendTag(val, true);
-// 		}
-// 		return;
-// 		// 下面不能有, 有就有问题
-// 		$("#addTagTrigger").show();
-// 		$("#addTagInput").hide();
-// 		// revertTagStatus();
-// 	});
-// 	$('#addTagInput').keydown(function(e) {
-// 		if (e.keyCode == 13) {
-// 			hideTagList();
-// 			// 如果有值, 再生成, 没值直接隐藏
-// 			if ($("#addTagInput").val()) {
-// 				$(this).trigger("blur");
-// 				$("#addTagTrigger").trigger("click");
-// 			} else {
-// 				$(this).trigger("blur");
-// 			}
-// 		}
-// 	});
-// 	// 点击下拉时也会触发input的blur事件
-// 	$("#tagColor li").click(function(event) {
-// 		var a;
-// 		if($(this).attr("role")) {
-// 			a = $(this).find("span");
-// 		} else {
-// 			a = $(this);
-// 		}
-// 		Tag.appendTag({
-// 			classes : a.attr("class"),
-// 			text : a.text()
-// 		}, true);
-// 	});
-// 	
-// 	$("#tags").on("click", "i", function() {
-// 		Tag.removeTag($(this).parent());
-// 	});
-//   
-// 	// tag nav
-// 	function deleteTag() {
-// 		$li = $(this).closest('li');
-// 		var tag = $.trim($li.data("tag"));
-// 		if(confirm(getMsg("Are you sure ?"))) {
-// 			TagService.deleteTag(tag, function(re) {
-// 				if(typeof re == "object" && re.Ok !== false) {
-// 					Note.deleteNoteTag(re, tag);
-// 					$li.remove();
-// 				}
-// 			});
-// 		};
-// 	}
-// 	
-// 	function searchTag() {
-// 		var $li = $(this).closest('li');
-// 		var tag = $.trim($li.data("tag"));
-// 		Tag.searchTag(tag);
-// 	};
-// 
-// 	$("#tagNav").on("click", "li .label", searchTag);	
-// 	$("#tagNav").on("click", "li .tag-delete", deleteTag);
-// });
+/**
+ * tag navigator
+ */
+var TagNav = function() {
+  this.tags = [];
+  this.curTag = null;
+  this.$element = $('#tagNav');
+  this.$element.on('click', 'li .label', function() {
+    var tagValue = $(this).closest('li').data('tag').trim();
+    this._searchByTag(tagValue);
+  });
+  this.$element.on('click', 'li .tag-delete', this._deleteTag);    
+};
+
+TagNav.prototype = {
+  constructor: TagNav,
+  _searchByTag: function(tag, noteId) {
+  	Note.curChangedSaveIt();
+  	// 清空所有，也会把curNoteId清空
+  	Note.clearAll();
+
+  	var tagTitle = '<span class="label label-default">' + trimTitle(tag) + '</span>';
+
+  	Notebook.changeCurNotebookTitle(tagTitle, false, '', true);
+    this.curTag = tag;
+
+  	NoteService.searchNoteByTag(tag, function(notes) {
+  		hideLoading();
+  		if(notes) {
+  			Note.renderNotes(notes);
+  			Note.renderNotesAndTargetNote(notes, noteId);
+  		}
+  	});
+  },
+  
+  _deleteTag: function() {
+    $li = $(this).closest('li');
+    var tag = $.trim($li.data('tag'));
+    if(confirm(getMsg('Are you sure ?'))) {
+      TagService.deleteTag(tag, function(re) {
+        if(typeof re == 'object' && re.Ok !== false) {
+          Note.deleteNoteTag(re, tag);
+          $li.remove();
+        }
+      });
+    };
+  },
+  
+  // called by node: web.js
+  deleteTags: function(tags) {
+    tags = tags || [];
+    for(var i = 0; i < tags.length; ++i) {
+      var title = tags[i];
+      $('#myTag li[data-tag="' + title + '"]').remove();
+    }
+  },
+
+  // called by node: web.js
+  // 添加标签，并重绘到左侧
+  addTags: function(tags) {
+    tags = tags || [];
+    // 去重，将添加的标签放在第一个位置
+    for(var i = 0; i < tags.length; ++i) {
+      for(var j in this.tags) {
+        if(this.tags[j].Tag == tags[i].Tag) {
+          this.tags.splice(j, 1);
+          break;
+        }
+      }
+      this.tags.unshift(tags[i]);
+    }
+    this.setTags(this.tags);
+  },
+
+  // called by page.js
+  // 更新tags，并重绘到左侧
+  setTags: function(tags) {
+  	this.tags = tags || [];
+  	$('#tagNav').html('');
+  	for(var i in this.tags) {
+  		var noteTag = this.tags[i];
+  		var tag = noteTag.Tag;
+  		var text = tag;
+  		
+  		text = trimTitle(text);
+  		var classes = 'label label-default';
+  		// 笔记数量先隐藏, 不准确
+  		$('#tagNav').append(tt('<li data-tag="?"><a> <span class="?">? <em style="display: none">(?)</em></span> <i class="tag-delete">X</i></li>', tag, classes, text, noteTag.Count));
+  	}
+
+  	if(this.tags.length == 0) {
+  		$('#tagNav').html('<p class="no-info">' + getMsg('No tag') + '</p>');
+  	}
+  },
+};
+
+
+/**
+ * tag edit area of editor
+ */
+var TagInput = function() {
+  var me = this;
+  me.tags = []; //array of strings
+  me.numSuggestions = 0;
+  me.selectedSuggestion = 0;
+  me.$tags = $("#tags");
+  me.$input = $("#tagInput");
+  me.$inputPrompt = $("#tagInputPrompt");
+  me.$suggestions = $("#tagSuggestion");
+  me._hideSuggestion();
+  
+  me.$inputPrompt.click(function(e) {
+    me.$inputPrompt.hide();
+    me.$input.show().focus().val("");
+  });
+  
+  // 保存tag
+  me.$input.blur(function() {
+    var val = me.$input.val();
+    me._hideSuggestion();
+    if(val) {
+      me.$input.val('');
+      me._addTag(val, true);
+    }
+  });
+  
+  me.$input.keydown(function(e) {
+    switch (e.keyCode) {
+      case 8:
+        me._onBackSpace(e);
+        break;
+      case 13:
+        me._onEnter(e);
+        break;
+      case 27:
+        me._onEscape(e);
+        break;
+      case 38:
+        me._onUpArrow(e);
+        break;
+      case 40:
+        me._onDownArrow(e);
+        break;
+      default:
+    }
+  });
+  
+  me.$input.on('input', function(e) {
+    if(me.timer) {
+      clearTimeout(me.timer);
+    }
+    me.timer = setTimeout(function() {
+      var val = me.$input.val();
+      if(val) {
+        me._showSuggestion(val);
+      }
+      else {
+        me._hideSuggestion();
+      }
+    }, 200);
+  });
+  
+  me.$tags.on('click', 'i', function() {
+    me._removeTag($(this).parent());
+  });
+
+  // 使用click事件的话，就无法阻止input的blur事件触发
+  me.$suggestions.on('mousedown', 'li', function(e) {
+    var $li = $(this);
+    me._addTag($li.text(), true);
+    e.preventDefault();
+    me.$input.val('');
+  });
+}
+
+TagInput.prototype = {
+  constructor: TagInput,
+  
+  // called by Note
+  getTags: function() {
+  	var tags = [];
+  	this.$tags.children().each(function(){
+  		var text = $(this).data('tag');
+  		tags.push(text);
+  	});
+  	// 正常情况下不会重复
+  	return tags;
+  },
+  
+  // called by Note
+  clearTags: function() {
+  	this.$tags.html('');
+  },
+
+  // called by Note
+  setTags: function(tags) {
+  	if(!Array.isArray(tags)) {
+  		return;
+  	}
+    this.$tags.html('');
+  	for(var i = 0; i < tags.length; ++i) {
+  		this._addTag(tags[i]);
+  	}
+  },
+  
+  // tag应该是string的，也可能是number
+  _addTag: function(tag, save) {
+    if(!tag) {
+      return;
+    }
+    var classes = "label label-default",
+      text = (tag + "").trim(),
+      escapedText = trimTitle(text);
+    
+    // 取出tags中重复
+    var duplicate = false;
+    this.$tags.children().each(function() {
+      if (escapedText + "X" == $(this).text()) {
+        $(this).remove();
+        duplicate = true;
+      }
+    });
+
+    this.$tags.append(tt('<span class="?" data-tag="?">?<i title="' + getMsg("delete") + '">X</i></span>', classes, text, escapedText));
+        
+    if(save && !duplicate) {
+      this._saveTag(text);
+    }
+  },
+
+  // 保存tag
+  _saveTag(text) {
+    if(text)
+    {
+      Note.curChangedSaveIt(true, function() {
+        TagService.addOrUpdateTag(text, function(ret) {
+          if(typeof ret == 'object' && ret.Ok !== false) {
+            Tag.nav.addTags([ret]);
+          }
+        });
+      });
+    }
+  },
+  
+  // 删除tag
+  _removeTag: function($target) {
+    var text = $target.data('tag');
+    $target.remove();
+    if(text) {
+      Note.curChangedSaveIt(true, function() {
+        TagService.addOrUpdateTag(text, function(ret) {
+          if(typeof ret == 'object' && ret.Ok !== false) {
+            // 刚删除的tag不需要放到nav的第一个
+            // Tag.nav.addTags([ret]);
+          }
+        });
+      });
+    }
+  },
+  
+  _showSuggestion: function(keyword) {
+    // alert("show suggest");
+    var me = this;
+    if(!keyword) {
+      return;
+    }
+    keyword = keyword.toLowerCase();
+    this.$suggestions.html('');
+    TagService.getTags(function(tags) {
+      var texts = tags
+        .map(function(tag) {
+          return tag.Tag + ''; 
+        })
+        .filter(function(text) {
+          return text.toLowerCase().startsWith(keyword);
+        })
+        .sort()
+        .slice(0, 6);
+      texts.forEach(function(text) {
+        me.$suggestions.append($(`<li>${text}</li>`));
+      });
+      
+      // placeholder tag that represent current input
+      me.$suggestions.prepend($(`<li class="hidden">${me.$input.val()}</li>`));
+      me.selectedSuggestion = 0;
+      me.numSuggestions = texts.length;
+      
+      if(texts.length > 0) {
+        $('#tagInputGroup').addClass('open');      
+      }
+      else {
+        $('#tagInputGroup').removeClass('open');      
+      }      
+    });
+  },
+  
+  _hideSuggestion: function() {
+    $('#tagInputGroup').removeClass('open');
+  },
+  
+  _selectSuggestion: function(index) {
+    if(this.numSuggestions) {
+      this.$suggestions.children().eq(this.selectedSuggestion).removeClass("selected");
+      var $suggestion = this.$suggestions.children().eq(index);      
+      $suggestion.addClass("selected");
+      this.$input.val($suggestion.text());
+      this.$input[0].setSelectionRange($suggestion.text().length, $suggestion.text().length);
+      this.selectedSuggestion = index;
+    }
+  },
+
+  _onBackSpace: function(e) {
+    var val = this.$input.val();
+    if(!val) {
+      this._removeTag(this.$tags.children().last());
+      e.preventDefault();
+    }
+  },
+
+  _onEnter: function(e) {
+    var val = this.$input.val();
+    this._hideSuggestion();
+    if(val) {
+      this.$input.val('');
+      this._addTag(val, true);
+    }
+  },
+
+  _onEscape: function(e) {
+    this.$input.val('');
+    this.$input.trigger('blur');
+  },
+
+  _onUpArrow: function(e) {
+    var index = (this.selectedSuggestion - 1) % (this.numSuggestions + 1);
+    this._selectSuggestion(index);
+    e.preventDefault();
+  },
+  
+  _onDownArrow: function(e) {
+    var index = (this.selectedSuggestion + 1) % (this.numSuggestions + 1);
+    this._selectSuggestion(index);
+    e.preventDefault();
+  }
+};
+
+Tag.nav = new TagNav();
+Tag.input = new TagInput();
