fea71f8be509b613bf658aa003926376bd38a6bd
life <lifephp@gmail.com>
2021-03-27 20:31:23 +0800

    Update gui.js

--- src/gui.js
+++ src/gui.js
@@ -1,27 +1,24 @@
-const {dialog, Menu, MenuItem, app} = require('@electron/remote')
+const { dialog, Menu, MenuItem, app } = require('@electron/remote')
 var remote = require('@electron/remote');
-const path = require('path')
 
-// var dialog = require('dialog');
-let _lastOpenPath = null
 var gui = {
-	Menu: Menu,
-	MenuItem: MenuItem,
-	remote: remote,
-	app: app,
-	dialog: dialog,
+    Menu: Menu,
+    MenuItem: MenuItem,
+    remote: remote,
+    app: app,
+    dialog: dialog,
 
-	getSeparatorMenu: function() {
-		return new MenuItem({ type: 'separator' });
-	},
-	win: remote.getCurrentWindow(),
-	getCurrentWindow: function() {
-		return remote.getCurrentWindow();
-	},
-	Shell: require('electron').shell,
-	on: function(type, callback) {
+    getSeparatorMenu: function() {
+        return new MenuItem({ type: 'separator' });
+    },
+    win: remote.getCurrentWindow(),
+    getCurrentWindow: function() {
+        return remote.getCurrentWindow();
+    },
+    Shell: require('electron').shell,
+    on: function(type, callback) {
 
-	}
+    }
 };
 
 module.exports = gui;
